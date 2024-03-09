8d5758f8ee4b049bf43b06a96c7fe33b427f4e12
Asbjørn Olav Orvedal <asbjorn.orvedal@gmail.com>
2024-03-09 18:22:45 +0100

    WIP: started implmeenting map and player

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,54 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Main where
 
 import Brick
-import Brick.Widgets.Border
-import Brick.Widgets.Border.Style
-import Brick.Widgets.Center
+import Brick.Widgets.Center (center)
+import Data.List.Split (chunksOf)
+import qualified Data.Map as Map
+import GHC.Arr
 import Graphics.Vty
 
-placeholder :: Widget n
-placeholder =
-    joinBorders
-        $ borderWithLabel
-            (str "Hello!")
-        $ withBorderStyle
-            unicode
-            ( center (str "Left")
-                <+> vBorder
-                <+> center (str "Right")
-                <=> hBorder
-                <=> center (str "Down")
-            )
+class Entity a where
+    position :: a -> Coordinate
+    draw :: a -> Widget n
 
-data GameState = GameState deriving (Show)
+type World = [Room]
+type Name = ()
+data Player = Player
+    { name :: String
+    , pos :: Coordinate
+    }
+    deriving (Show)
+data Room = Room
+    { cells :: Array Coordinate Cell
+    , entities :: Map.Map Coordinate Player
+    }
 
-app :: App GameState () String
+instance Entity Player where
+    position (Player _ pos) = pos
+    draw _ = str "😎"
+
+type Coordinate = (Int, Int)
+
+data Cell = Floor | Wall
+
+instance Show Cell where
+    show Floor = ".."
+    show Wall = "##"
+
+data GameState = GameState
+    { player :: Player
+    , world :: World
+    }
+
+app :: App GameState () Name
 app =
     App
-        { appDraw = const [placeholder]
+        { appDraw = drawGame
         , appChooseCursor = neverShowCursor
         , appHandleEvent = \case
             VtyEvent e -> case e of
@@ -39,8 +59,36 @@ app =
         , appAttrMap = \_ -> attrMap defAttr []
         }
 
+emptyRoom :: Room
+emptyRoom =
+    Room
+        { cells = listArray ((0, 0), (9, 9)) (replicate 100 Floor)
+        , entities = Map.empty
+        }
+
+drawGame :: GameState -> [Widget Name]
+drawGame g@(GameState {world}) = [center $ vBox $ drawRoom g]
+
+drawRoom :: GameState -> [Widget Name]
+drawRoom g@(GameState {world, player}) = [vBox (hBox <$> rows)]
+  where
+    rows = chunksOf 10 $ do
+        (coord, cell) <- assocs $ cells $ head world
+        let ents = entities $ head world
+            x = Map.lookup coord ents
+        return $
+            if pos player == coord
+                then draw player
+                else case x of
+                    Just p -> str (show p)
+                    Nothing -> str (show cell)
+
+-- drawRoom Room {cells, entities} = [vBox (hBox <$> rows)]
+--   where
+--     rows = chunksOf 10 $ elems (str . show <$> cells)
+
 main :: IO ()
 main = do
-    let initialState = GameState
+    let initialState = GameState (Player "Mr. Bean" (0, 0)) [emptyRoom]
     finalState <- defaultMain app initialState
-    print finalState
+    print "hei"
