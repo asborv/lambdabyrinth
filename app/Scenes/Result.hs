4b39bc00b4d56cd15fa758ce142d6e4fded3edb7
Asbjørn Olav Orvedal <asbjorn.orvedal@gmail.com>
2024-03-29 11:49:10 +0100

    feat: Added game over screen

--- app/Scenes/Result.hs
+++ app/Scenes/Result.hs
@@ -2,5 +2,48 @@ module Scenes.Result where
 
 import Scenes.Game (GameState)
 
+import Brick
+    ( App (..)
+    , BrickEvent (VtyEvent)
+    , Widget
+    , attrMap
+    , defaultMain
+    , halt
+    , txt
+    , vBox
+    , (<=>)
+    )
+import Brick.Main (neverShowCursor)
+import Brick.Widgets.Border (borderWithLabel)
+import Brick.Widgets.Center (center, hCenter, vCenter)
+import Data.Text (Text)
+import Graphics.Vty (Event (..), defAttr)
+import Scenes.Scene (Name, Scene)
+
+app :: Scene ()
+app =
+    App
+        { appDraw = drawScene
+        , appChooseCursor = neverShowCursor
+        , appHandleEvent = \case
+            VtyEvent e -> case e of
+                EvKey _ [] -> halt
+                _ -> return ()
+            _ -> return ()
+        , appStartEvent = return ()
+        , appAttrMap = const $ attrMap defAttr []
+        }
+
+drawScene :: () -> [Widget Name]
+drawScene _ =
+    [ borderWithLabel (txt "Results") $
+        vCenter $
+            centerLines ["💀 YOU DIED!", "Press any key to quit"]
+    ]
+
 showResult :: GameState -> IO ()
-showResult = print
+showResult = return $ defaultMain app ()
+
+-- | Take a list of 'Text's, and center each in a vertical list
+centerLines :: [Text] -> Widget Name
+centerLines = vBox . fmap (hCenter . txt)
