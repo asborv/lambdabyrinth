54975811304641d154c60861407a8a8a3f97407e
Asbjørn Olav Orvedal <asbjorn.orvedal@gmail.com>
2024-06-09 20:09:58 +0200

    fix: Fixed bug where monsters may spawn on top of stairs

--- src/Utils.hs
+++ src/Utils.hs
@@ -8,3 +8,9 @@ tshow = pack . show
 
 guarded :: Monad m => m Bool -> m () -> m ()
 guarded condition action = condition >>= flip when action
+
+{- | Count the number of occurrences of a specific element in a list
+Courtesy of: https://stackoverflow.com/questions/19554984/haskell-count-occurrences-function
+-}
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (== x)
